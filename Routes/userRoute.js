3e8b343013e6736f4820f15728b03807fb1aad13
LiponRoy <liponbdbd@gmail.com>
2024-03-05 18:00:39 +0600

    [Done] role based authorization

--- Routes/userRoute.js
+++ Routes/userRoute.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { allUsers, singleUser, userDelete, userUpdate } from "../Controllers/userController.js";
-import { isAuth } from "../auth/verifytoken.js";
+import { isAuth, restrictArea } from "../auth/verifytoken.js";
 
 
 
@@ -9,7 +9,7 @@ const router = express.Router();
 router.put("/:id",userUpdate);
 router.delete("/:id",userDelete);
 router.get("/:id",singleUser);
-router.get("/",isAuth,allUsers);
+router.get("/",isAuth,restrictArea(["patient"]), allUsers);
 
 
 export default router;
--- auth/verifyToken.js
+++ auth/verifyToken.js
@@ -1,18 +1,48 @@
+import jwt from "jsonwebtoken";
+import UserSchema from "../models/UserSchema.js";
+import DoctorSchema from "../models/DoctorSchema.js";
+
+export const isAuth = (req, res, next) => {
+  const authToken = req.headers.authorization;
+
+  if (!authToken || !authToken.startsWith("Bearer")) {
+    return res.status(403).json({ message: "No Token no authorization" });
+  }
+
+  try {
+    const token = authToken.split(" ")[1];
+    const decode = jwt.verify(token, process.env.JWT_SECRET);
+    req.userId = decode.id;
+    req.role = decode.role;
+    next();
+  } catch (error) {
+    if (error.name === "TokenExpiredError") {
+      return res.status(403).json({ message: "Token has Expired" });
+    }
+    return res.status(403).json({ message: "Invalid Token" });
+  }
+};
 
 
-export const isAuth=(req,res,next) =>{
-    const token = req.headers.authorization;
 
-    if(!token || !token.startsWith("Bearer")){
-        return res.status(403).json({message:"No Token no authorization"})
+export const restrictArea = (roles) =>async(req,res,next)=>{
+    const userId = req.userId;
+    console.log("uid ",userId);
+    let user;
+  
+    const patient =await UserSchema.findById(userId);
+    const doctor =await DoctorSchema.findById(userId);
+  
+    if (patient) {
+      user = patient;
+    } else if (doctor) {
+      user = doctor;
+    }
+  
+    if (!roles.includes(user.role)) {
+      return res.status(403).json({success:"false", message: "You are not Authorized person"});
     }
 
-    try {
-        console.log(token)
-        next();
-    } catch (error) {
-        console.log(error)
-    }
-
+    next();
 
 }
